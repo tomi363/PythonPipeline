c933f11bb81f206dd1e5ceb1531da0d940e0609e
zakan <tomi@smartlabs.be>
2025-06-30 22:57:49 +0300

    fixed imports

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import main
 from unittest import mock
 from pynput.keyboard import KeyCode
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_toggle_spamming():
