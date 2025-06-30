7d4e459f41b2254ec9b57ff11fbcc120ccbc1a6d
zakan <tomi@smartlabs.be>
2025-06-30 23:17:47 +0300

    ignore lint error

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,11 @@
 import sys
 import os
 
-
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-)
-
+)  # noqa: E402
 
 import main
-
 from unittest import mock
 from pynput.keyboard import KeyCode
 
