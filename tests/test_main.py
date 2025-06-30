10fb16c1e8ccd57a2de7f0d8178e90a48252e4a4
zakan <tomi@smartlabs.be>
2025-06-30 23:20:23 +0300

    ignore lint error

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,10 @@
+# flake8: noqa: E402
 import sys
 import os
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-)  # noqa: E402
+)
 
 import main
 from unittest import mock
